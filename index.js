fd694be4e12f08aad40318f1203464430550220e
Andrea Cox <msandrea712@yahoo.com>
2023-06-03 11:25:53 -0400

    getter and setter

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this._radius = radius;
+  }
+
+  get radius() {
+    return this._radius;
+  }
+
+  get diameter() {
+    return this._radius * 2;
+  }
+
+  get circumference() {
+    return 2 * Math.PI * this._radius;
+  }
+
+  get area() {
+    return Math.PI * this._radius ** 2;
+  }
+
+  set radius(value) {
+    this._radius = value;
+  }
+
+  set diameter(value) {
+    this._radius = value / 2;
+  }
+
+  set circumference(value) {
+    this._radius = value / (2 * Math.PI);
+  }
+
+  set area(value) {
+    this._radius = Math.sqrt(value / Math.PI);
+  }
+}
